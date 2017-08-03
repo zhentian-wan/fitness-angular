9c6f2418a0ca1a363a08625546d479f730b1d147
Zhentian Wan <answer881215@gmail.com>
2017-08-03 22:12:10 +0300

    add modules and fix travis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { Store } from 'store';
 
 // containers
 import { AppComponent } from './containers/app/app.component';
+import {AuthModule} from '../auth/auth.module';
 
 // components
 
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
---
+++ src/auth/login/login.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+
+import { LoginComponent } from './containers/login/login.component';
+
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES)
+  ],
+  declarations: [
+    LoginComponent
+  ]
+})
+export class LoginModule {}
