a3a572a4a9e5f3795dcf571db4c516a2d8520d90
Zhentian Wan <answer881215@gmail.com>
2017-08-06 12:19:56 +0300

    reactive stor and AngularFire auth

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from 'store';
+import {AuthService} from '../../../auth/shared/services/auth/auth.service';
+
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import {User} from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
+      <h1>{{user$ | async | json}}</h1>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy{
+
+  user$: Observable<User>;
+  subscription: Subscription;
+
+  constructor(
+    private store: Store,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<User>('user');
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,11 +1,37 @@
 
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
+import {Store} from 'store';
+
+import 'rxjs/add/operator/do';
+
+export interface User {
+  uid: string;
+  email: string;
+  authenticated: boolean;
+}
 
 @Injectable()
 export class AuthService {
+
+  // handle on every auth state changes
+  auth$ = this.af.authState
+    .do(next => {
+      if (!next) {
+        this.store.set('user', null);
+        return;
+      }
+      const user = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true
+      };
+      this.store.set('user', user);
+    });
+
   constructor(
-    private af: AngularFireAuth
+    private af: AngularFireAuth,
+    private store: Store
   ) {
 
   }
